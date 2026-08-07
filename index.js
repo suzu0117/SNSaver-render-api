6de913309121d9cbea1345c6f06c25582cfe585e
example <example@gmail.com>
2026-08-08 07:16:02 +0900

    test

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ import path from "path";
 const app = express();
 app.use(
     cors({
-        origin: "https://snsaver-web.onrender.com",
+        origin: "https://snsaver.onrender.com",
     }),
 );
 app.use(express.json());
