1aa447fb295cf3c165c0ae5490070169f6515791
example <example@gmail.com>
2026-08-08 06:47:16 +0900

    test

---
+++ index.js
@@ -0,0 +1,17 @@
+import "dotenv/config";
+import express from "express";
+import cors from "cors";
+import { router } from "./router.js";
+import path from "path";
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.use("/api", router);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`);
+});
---
+++ router.js
@@ -0,0 +1,45 @@
+import "dotenv/config";
+import express from "express";
+
+const router = express.Router();
+
+router.post("/search", async (req, res) => {
+    try {
+        const username = req.body.username;
+
+        const response = await fetch(`${process.env.API}/api/search`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ username: username }),
+        });
+        console.log(response.status);
+        const json = await response.json();
+        return res.status(response.status).json(json);
+    } catch (error) {
+        console.log(error);
+        res.status(503).json({
+            message: "service unavailable",
+            data: null,
+        });
+    }
+});
+
+router.get("/status/:id", async (req, res) => {
+    try {
+        const id = req.params.id;
+        const response = await fetch(`${process.env.API}/api/status/${id}`);
+        const json = await response.json();
+        res.status(response.status).json(json);
+    } catch (error) {
+        console.log(error);
+        res.status(503).json({
+            message: "service unavailable",
+            status: null,
+            url: null,
+        });
+    }
+});
+
+export { router };
