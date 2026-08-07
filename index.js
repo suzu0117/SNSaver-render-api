56ebcd36affdc23c8e55a98b790e375256d61dda
example <example@gmail.com>
2026-08-08 06:54:50 +0900

    test

--- index.js
+++ index.js
@@ -5,10 +5,12 @@ import { router } from "./router.js";
 import path from "path";
 
 const app = express();
-
+app.use(
+    cors({
+        origin: "https://snsaver-web.onrender.com",
+    }),
+);
 app.use(express.json());
-app.use(cors());
-
 app.use("/api", router);
 
 const PORT = process.env.PORT || 3000;
